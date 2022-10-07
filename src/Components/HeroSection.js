7df43c763407c11b91920cc754b709d99a1d0d47
Mohan Sai <potlamohansai@gmail.com>
2022-10-07 15:28:59 +0530

    theme switcher

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,10 @@
+import React, {useContext} from "react";
+import ThemeContext from "../Context/ThemeContext"
+import AppTheme from "../Colors"
+
+
+const HeroSection = () => {
+    const theme = useContext(ThemeContext)
+}
+
+export default HeroSection

a87d046368e800676cf7b84e927a84eb66260aec
Mohan Sai Potla <potlamohansai@gmail.com>
2022-10-08 21:00:10 +0530

    final changes

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -18,7 +18,11 @@ const HeroSection = () => {
         }}
         >
             <h1>Context API Theme toggler</h1>
-            <p>This is a nice paragraph</p>
+            <p>Context provides a way to pass data through the component tree without having to pass props down manually at every level.</p>
+            <p style={{margin: "20px 200px"}}>In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.</p>
+            <p style={{margin: "20px 200px"}}>In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.</p>
+            <p style={{margin: "20px 200px"}}>In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.</p>
+            <p style={{margin: "20px 200px"}}>In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.</p>
             <button
                 style ={{
                     backgroundColor: "#26ae60",
